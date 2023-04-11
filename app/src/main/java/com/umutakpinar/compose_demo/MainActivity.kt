401d362d093f334012273f57234a7c3675d21b5d
Umut <umtakpnr5@gmail.com>
2023-04-11 03:32:56 +0300

    Initial commit of compose learning demo

---
+++ app/src/main/java/com/umutakpinar/compose_demo/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.umutakpinar.compose_demo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.umutakpinar.compose_demo.ui.theme.Compose_demoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Compose_demoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+    
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Compose_demoTheme {
+        Greeting("Android")
+    }
+}
