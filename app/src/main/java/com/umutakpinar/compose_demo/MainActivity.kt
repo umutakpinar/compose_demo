e663eeb5d3a6ff170ba1de7d085d86e76c949f8e
Umut <umtakpnr5@gmail.com>
2023-04-23 18:38:04 +0300

    DrawerMenu icon shows on HomeScreen and it can be open drawer

--- app/src/main/java/com/umutakpinar/compose_demo/MainActivity.kt
+++ app/src/main/java/com/umutakpinar/compose_demo/MainActivity.kt
@@ -16,36 +16,40 @@ import androidx.compose.material.ScaffoldState
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.outlined.ArrowBack
+import androidx.compose.material.icons.outlined.Menu
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.currentRecomposeScope
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.umutakpinar.compose_demo.navigation.Screen
 import com.umutakpinar.compose_demo.navigation.SetupNavGraph
 import com.umutakpinar.compose_demo.ui.theme.Compose_demoTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
     private lateinit var navController: NavHostController
+    private lateinit var scaffoldState: ScaffoldState
+    private lateinit var coroutineScope: CoroutineScope
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Compose_demoTheme {
                 navController = rememberNavController()
-                val scaffoldState = rememberScaffoldState()
-                MainScreen(navController = navController, scaffoldState)
+                scaffoldState = rememberScaffoldState()
+                coroutineScope = rememberCoroutineScope()
+                MainScreen(navController = navController, scaffoldState, coroutineScope)
             }
         }
     }
@@ -55,11 +59,13 @@ class MainActivity : ComponentActivity() {
 fun MainScreen(
     navController: NavHostController,
     scaffoldState: ScaffoldState,
+    coroutineScope: CoroutineScope,
 ) {
     //Taking currentBackStackEntry as state so we can observe the changes
     val navBackStackEntry by navController.currentBackStackEntryAsState()
     val currentRoute = navBackStackEntry?.destination?.route
     Scaffold(
+        scaffoldState = scaffoldState,
         topBar = {
             TopAppBar(
                 backgroundColor = MaterialTheme.colors.background
@@ -72,16 +78,27 @@ fun MainScreen(
                     Box(
                         modifier = Modifier.fillMaxSize()
                     ){
-                        if (currentRoute != Screen.Home.route) {
-                            IconButton(
-                                modifier = Modifier.align(Alignment.CenterStart),
-                                onClick = {
-                                navController.popBackStack()
-                            }) {
-                                Icon(
-                                    imageVector = Icons.Outlined.ArrowBack,
-                                    contentDescription = "Go back"
-                                )
+                        Box(modifier = Modifier.align(Alignment.CenterStart),){
+                            if (currentRoute != Screen.Home.route) {
+                                IconButton(
+                                    onClick = {
+                                        navController.popBackStack()
+                                    }) {
+                                    Icon(
+                                        imageVector = Icons.Outlined.ArrowBack,
+                                        contentDescription = "Go back button"
+                                    )
+                                }
+                            }else{
+                                IconButton(onClick = {
+                                    //Change here when you added viewModel
+                                    //Change the dispatcher if its wrong I dont know which one is true one for process like opening drawer
+                                    coroutineScope.launch {
+                                        scaffoldState.drawerState.open()
+                                    }
+                                }) {
+                                    Icon(imageVector = Icons.Outlined.Menu, contentDescription = "Menu button")
+                                }
                             }
                         }
                         Text(
@@ -92,6 +109,9 @@ fun MainScreen(
                 }
 
             }
+        },
+        drawerContent = {
+            Box(modifier = Modifier.fillMaxSize())
         }
     ) {
         Box(modifier = Modifier.padding(it)) {
@@ -110,9 +130,11 @@ fun DefaultPreview() {
             mutableStateOf(false)
         }
         val scaffoldState = rememberScaffoldState()
+        val coroutineScope = rememberCoroutineScope()
         MainScreen(
             navController = rememberNavController(),
-            scaffoldState = scaffoldState
+            scaffoldState = scaffoldState,
+            coroutineScope = coroutineScope
         )
     }
 }
